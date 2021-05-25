29d93b394933ee5cd8720846248bca94dbabd00a
easylight <easylightcheckbox@gmail.com>
2021-05-25 16:44:54 +0300

    Работают картинки

--- demo/demo/service/models.py
+++ demo/demo/service/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.urls import reverse
-
+from django.core.files.storage import FileSystemStorage
 
 class Service(models.Model):
     name = models.CharField('Название услуги', max_length=200)
     category = models.CharField('Категория услуг', max_length=200)
     description = models.TextField('Описание услуги')
     price = models.IntegerField('Цена', blank=False)
-    pub_date = models.DateTimeField('Дата публикации')
+    photo = models.ImageField(upload_to='transfers')
 
     def __str__(self):
         return '{}'.format(self.name)
--- demo/demo/service/urls.py
+++ demo/demo/service/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import ServiceListView, AboutTemplateView, ServiceDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', ServiceListView.as_view(), name='home'),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
