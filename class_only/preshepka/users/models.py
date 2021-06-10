65078f5d9a1de24d91ba08e3525792c5af12fdd0
Dmitri <massliveg@gmail.com>
2021-06-10 12:58:58 +0300

    Написал модель инфо о пользователе. Миграция не сделана

--- class_only/preshepka/users/models.py
+++ class_only/preshepka/users/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class UserInfo(models.Model):
+    image = models.ImageField(upload_to='user', null=True)
+    phone = models.CharField(max_length=20, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, nul=True)
 
 
