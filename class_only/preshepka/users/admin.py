1bd9ebeb1af6a5e5e14f97634db063636919f8e2
Dmitri <massliveg@gmail.com>
2021-06-10 13:22:14 +0300

    Готов кабинет пользователя

--- class_only/preshepka/users/admin.py
+++ class_only/preshepka/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from .models import UserInfo
+
+admin.site.register(UserInfo)
 
 
 
--- class_only/preshepka/users/models.py
+++ class_only/preshepka/users/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class UserInfo(models.Model):
     image = models.ImageField(upload_to='user', null=True)
     phone = models.CharField(max_length=20, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, nul=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
 
